99359efbfe3555a2507f165ca1ff300acac52760
smzyuuuu <smzyuuuu@gmail.com>
2021-07-02 23:15:42 +0900

    Initial Commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by Yuu Shimizu on 2021/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Yuu Shimizu on 2021/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
