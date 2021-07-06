4e67fb215329182ba897d6291fca9c6cde616db4
smzyuuuu <smzyuuuu@gmail.com>
2021-07-06 23:40:49 +0900

    my janken first commit

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var answerNumber = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            if answerNumber == 0 {
+                Text("これからじゃんけんをします")
+                    .padding(.bottom)
+            } else if answerNumber == 1 {
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("グー")
+                    .padding(.bottom)
+            } else if answerNumber == 2 {
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("チョキ")
+                    .padding(.bottom)
+            } else {
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("パー")
+                    .padding(.bottom)
+            }
+            
+            Button(action: {
+                answerNumber = Int.random(in: 1...3)
+                
+                var newAnswerNum = 0
+                
+                repeat {
+                    newAnswerNum = Int.random(in: 1...3)
+                } while answerNumber == newAnswerNum
+                
+                answerNumber = newAnswerNum
+            }){
+                Text("じゃんけんをする！")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
+            }
+        }
     }
 }
 
